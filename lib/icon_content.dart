f65cecc31ef917968c95d362acaf84eec57030c3
BadenX <188515695+BadenX@users.noreply.github.com>
2026-03-04 22:03:22 +0100

    Slider and custom button +Middle slider with custom settings +Bottom widgets +Custom buttons for the 2 widgets on the bottom +Separate file for constants

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/constants.dart';
 import 'package:flutter/material.dart';
 
 class IconContent extends StatelessWidget {
@@ -13,7 +14,7 @@ class IconContent extends StatelessWidget {
       children: [
         Icon(icon, size: 80.0),
         SizedBox(height: 15.0),
-        Text(label, style: TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98))),
+        Text(label, style: kLabelTextStyle),
       ],
     );
   }
